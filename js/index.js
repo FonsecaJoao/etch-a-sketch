95621ee9580a8b53227b80a388a585fc1f6be9a4
JoaoFonseca <jmgf_13@hotmail.com>
2019-03-07 15:02:37 +0000

    Refactor build function to suppport odd numbers

--- js/index.js
+++ js/index.js
@@ -6,33 +6,29 @@ const pixel = document.getElementById('pixel');
 
 function buildGrid() {
     grid.innerHTML = '';
-    if (pixel.value % 2 !== 0) {
-        alert('Select only even numbers please!');
-    } else {
-        const margin = pixel.value * 2;
-        const border = pixel.value * 2;
-        const sumMarginAndBorder = margin + border;
-        const fitInGrid = 500 - sumMarginAndBorder;
-        const measurePixel = fitInGrid / pixel.value;
-        const sqrLength = pixel.value * pixel.value;
-        let div = document.createElement('div');
-        for (let i = 0; i < sqrLength; i++) {
-            div = document.createElement('div');
-            grid.appendChild(div);
-            div.className = 'sqr';
-            div.setAttribute('id', 'sqr');
-            div.addEventListener('mouseover', hover);
-        }
-        for (let i = 0; i < sqrLength; i++) {
-            const sqr = document.querySelectorAll('#sqr');
-            sqr[i].style.height = '' + measurePixel + 'px';
-            sqr[i].style.width = '' + measurePixel + 'px';
-        }
+    const margin = pixel.value * 2;
+    const border = pixel.value * 2;
+    const sumMarginAndBorder = margin + border;
+    const fitInGrid = 500 - sumMarginAndBorder;
+    const measurePixel = fitInGrid / pixel.value;
+    const sqrLength = pixel.value * pixel.value;
+    let div = document.createElement('div');
+    for (let i = 0; i < sqrLength; i++) {
+        div = document.createElement('div');
+        grid.appendChild(div);
+        div.className = 'sqr';
+        div.setAttribute('id', 'sqr');
+        div.addEventListener('mouseover', hover);
+    }
+    for (let i = 0; i < sqrLength; i++) {
+        const sqr = document.querySelectorAll('#sqr');
+        sqr[i].style.height = '' + measurePixel.toFixed(2) + 'px';
+        sqr[i].style.width = '' + measurePixel.toFixed(2) + 'px';
     }
 };
 
 function hover() {
-    this.style.background = 'black';
+    this.style.backgroundColor = 'black';
 }
 
 function clearGrid() {

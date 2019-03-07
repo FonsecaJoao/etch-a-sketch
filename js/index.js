e69a9d2a18880964e4a9791fa464f82868e1fb4c
JoaoFonseca <jmgf_13@hotmail.com>
2019-03-07 14:33:12 +0000

    Refactor build function

--- js/index.js
+++ js/index.js
@@ -1,14 +1,33 @@
-const grid = document.getElementById('grid');
+let grid = document.getElementById('grid');
 const clear = document.getElementById('clear');
+const pixel = document.getElementById('pixel');
+
+
 
 function buildGrid() {
-    let div = document.createElement('div');
-    for (let i = 0; i < 256; i++) {
-        div = document.createElement('div');
-        grid.appendChild(div);
-        div.className = 'sqr';
-        div.setAttribute('id', 'sqr');
-        div.addEventListener('mouseover', hover);
+    grid.innerHTML = '';
+    if (pixel.value % 2 !== 0) {
+        alert('Select only even numbers please!');
+    } else {
+        const margin = pixel.value * 2;
+        const border = pixel.value * 2;
+        const sumMarginAndBorder = margin + border;
+        const fitInGrid = 500 - sumMarginAndBorder;
+        const measurePixel = fitInGrid / pixel.value;
+        const sqrLength = pixel.value * pixel.value;
+        let div = document.createElement('div');
+        for (let i = 0; i < sqrLength; i++) {
+            div = document.createElement('div');
+            grid.appendChild(div);
+            div.className = 'sqr';
+            div.setAttribute('id', 'sqr');
+            div.addEventListener('mouseover', hover);
+        }
+        for (let i = 0; i < sqrLength; i++) {
+            const sqr = document.querySelectorAll('#sqr');
+            sqr[i].style.height = '' + measurePixel + 'px';
+            sqr[i].style.width = '' + measurePixel + 'px';
+        }
     }
 };
 
@@ -19,10 +38,8 @@ function hover() {
 function clearGrid() {
     const sqr = document.querySelectorAll('#sqr');
     for (let i = 0; i <  sqr.length; i++) {
-        sqr[i].style.background = 'aquamarine'; 
+        sqr[i].style.backgroundColor = 'aquamarine';
     }
 }
 
 clear.addEventListener('click', clearGrid);
-
-buildGrid();
